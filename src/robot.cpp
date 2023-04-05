47fcd2b203d8a73284310490965d865b25bdc853
Johan Lind <Github@mormert.com>
2023-04-06 00:20:13 +0200

    Always transmit robotpos at 60 Hz

--- src/robot.cpp
+++ src/robot.cpp
@@ -184,10 +184,10 @@ Robot::update()
         auto pos = getPosition();
         j["pos"] = {{"x", pos.x}, {"y", pos.y}, {"r", body->GetAngle()}};
 
-        if(body->GetAngularVelocity() > 0.01f || body->GetLinearVelocity().Length() > 0.01f)
-        {
+        //if(body->GetAngularVelocity() > 0.01f || body->GetLinearVelocity().Length() > 0.01f)
+        //{
             Mqtt::getInstance().send("sim/out/robotpos", "RobotPos", j);
-        }
+        //}
     }
 
     lidarSensor->setPosition(getPosition());
